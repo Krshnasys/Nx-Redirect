8776547d4b54d2d91de641eed7bf194d722e388f
pompomxyz <freestff0077@gmail.com>
2025-04-18 16:02:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/health', (req, res) => {
-  res.status(200).send('Server is running');
+  res.status(200).send('Health check: OK');
 });
 
 app.get('/', (req, res) => {
